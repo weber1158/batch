f45833664e01adce945db367e214110c21d99559
Austin M. Weber <133900499+weber1158@users.noreply.github.com>
2025-02-03 19:06:44 -0500

    Update
    The previous version was not working as expected. The modifications here print more than just the Wifi password, but the password is printed in the "Key Content:" line of the output.

--- WifiPassword.bat
+++ WifiPassword.bat
@@ -1,33 +1,19 @@
 @echo off
-setlocal
 
-:: Prints the password to the current Wifi network.
-:: The user must be connected to the network in order
-:: for this script to work. If the user is not on Wifi
-:: then this script will say "~1".
+:: Print the Wifi password. You must be
+:: connected to the Wifi for this to work.
+:: You also need to know the name of the
+:: Wifi network.
 
-rem Get the name of the current Wi-Fi network
-for /f "tokens=2 delims=: " %%i in ('netsh wlan show interfaces ^| findstr /c:"SSID"') do set "ssid=%%i"
+:: If done correctly, the Command Prompt
+:: will print several lines. The Wifi pass-
+:: word will be listed under the "Security
+:: Settings" section next to "Key Content".
 
-rem Remove leading spaces from the SSID
-set "ssid=%ssid:~1%"
+set \p wifiName="Enter the name of the Wifi network that you are connected to: "
 
-rem Check if SSID is empty
-if "%ssid%"=="" (
-    echo You are not connected to a Wi-Fi network.
-    endlocal
-    pause
-    exit /b
-)
+NETSH WLAN SHOW PROFILE %wifiName% KEY=CLEAR
 
-rem Get the Wi-Fi password for the current network
-for /f "tokens=2 delims=: " %%i in ('netsh wlan show profile name^="%ssid%" key^=clear ^| findstr /c:"Key Content"') do set "password=%%i"
+echo The Wifi password is listed as Key Content.
 
-rem Remove leading spaces from the password
-set "password=%password:~1%"
-
-rem Print the Wi-Fi password
-echo The password for the Wi-Fi network "%ssid%" is: %password%
-
-endlocal
 pause
